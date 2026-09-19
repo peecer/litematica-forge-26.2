f4f82a898ae7ca008e2395f6c7312e48dcfa913d
peecer <cmkap23@gmail.com>
2026-09-19 12:53:35 -0400

    Fix bootstrap variable initialization

--- scripts/bootstrap-ci.sh
+++ scripts/bootstrap-ci.sh
@@ -10,7 +10,9 @@ work="$(mktemp -d)"
 trap 'rm -rf "$work"' EXIT
 
 fetch_source() {
-  local project="$1" file="$2" out="$3"
+  local project="$1"
+  local file="$2"
+  local out="$3"
   local url="https://www.curseforge.com/api/v1/mods/${project}/files/${file}/download"
   echo "Downloading $url"
   curl --fail --location --retry 4 --retry-all-errors --connect-timeout 30 "$url" -o "$out"
@@ -18,12 +20,15 @@ fetch_source() {
 }
 
 stage_source() {
-  local module="$1" jar="$2" unpack="$work/unpack-$module"
+  local module="$1"
+  local jar="$2"
+  local unpack="$work/unpack-$module"
   rm -rf "$module/src/main" "$unpack"
   mkdir -p "$module/src/main/java" "$module/src/main/resources" "$unpack"
   unzip -q "$jar" -d "$unpack"
   while IFS= read -r -d '' f; do
-    rel="${f#${unpack}/}"
+    local rel="${f#${unpack}/}"
+    local dest
     if [[ "$rel" == *.java ]]; then
       dest="$module/src/main/java/$rel"
     else
